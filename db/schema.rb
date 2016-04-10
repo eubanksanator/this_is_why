e0baf63f49a9574bcd1bbb6a25847f07b2ae79af
Travis Eubanks <tmeubanks113@gmail.com>
2016-04-10 08:58:58 -0500

    phase1 complete; adds posts for user and sets homepage

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160410131518) do
+ActiveRecord::Schema.define(version: 20160410133012) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
